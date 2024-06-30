cd7f1608760571ad7523146487ed9d61ae2a16aa
smhaf <sheikhhafizuddin019@gmail.com>
2024-06-30 16:07:24 +0800

    tes4

--- carculateApp/Form1.Designer.vb
+++ carculateApp/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
@@ -44,11 +45,20 @@ Partial Class Form1
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "test2"
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(69, 155)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Abu"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -60,4 +70,5 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
