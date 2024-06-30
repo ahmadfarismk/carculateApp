308e2dcfed2534b01a2606973e8afd71767361ef
smhaf <sheikhhafizuddin019@gmail.com>
2024-06-30 21:16:18 +0800

    buang

--- carculateApp/carSelect.Designer.vb
+++ carculateApp/carSelect.Designer.vb
@@ -27,16 +27,16 @@ Partial Class carSelect
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblCC = New System.Windows.Forms.Label()
+        Me.lblCost = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.lblPercent = New System.Windows.Forms.TextBox()
         Me.lblCompPay = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.RadioButton3 = New System.Windows.Forms.RadioButton()
@@ -51,7 +51,7 @@ Partial Class carSelect
         'pctCar
         '
         Me.pctCar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.pctCar.Location = New System.Drawing.Point(318, 27)
+        Me.pctCar.Location = New System.Drawing.Point(237, 23)
         Me.pctCar.Name = "pctCar"
         Me.pctCar.Size = New System.Drawing.Size(100, 77)
         Me.pctCar.TabIndex = 0
@@ -93,54 +93,54 @@ Partial Class carSelect
         Me.Label4.TabIndex = 4
         Me.Label4.Text = "Insurance Cost"
         '
-        'Label5
+        'lblPrice
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(161, 37)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 13)
-        Me.Label5.TabIndex = 5
-        Me.Label5.Text = "Label5"
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(161, 37)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(23, 13)
+        Me.lblPrice.TabIndex = 5
+        Me.lblPrice.Text = "Rm"
         '
-        'Label6
+        'lblYear
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(161, 73)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
-        Me.Label6.TabIndex = 6
-        Me.Label6.Text = "Label6"
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Location = New System.Drawing.Point(161, 73)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(14, 13)
+        Me.lblYear.TabIndex = 6
+        Me.lblYear.Text = "Y"
         '
-        'Label7
+        'lblCC
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(161, 111)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
-        Me.Label7.TabIndex = 7
-        Me.Label7.Text = "Label7"
+        Me.lblCC.AutoSize = True
+        Me.lblCC.Location = New System.Drawing.Point(161, 111)
+        Me.lblCC.Name = "lblCC"
+        Me.lblCC.Size = New System.Drawing.Size(14, 13)
+        Me.lblCC.TabIndex = 7
+        Me.lblCC.Text = "C"
         '
-        'Label8
+        'lblCost
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(161, 145)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(39, 13)
-        Me.Label8.TabIndex = 8
-        Me.Label8.Text = "Label8"
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Location = New System.Drawing.Point(161, 145)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(21, 13)
+        Me.lblCost.TabIndex = 8
+        Me.lblCost.Text = "Ins"
         '
         'Panel1
         '
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel1.Controls.Add(Me.Label5)
-        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.lblPrice)
+        Me.Panel1.Controls.Add(Me.lblCost)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.lblCC)
         Me.Panel1.Controls.Add(Me.Label2)
-        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.lblYear)
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.Label4)
-        Me.Panel1.Location = New System.Drawing.Point(249, 135)
+        Me.Panel1.Location = New System.Drawing.Point(168, 131)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(250, 200)
         Me.Panel1.TabIndex = 9
@@ -148,7 +148,7 @@ Partial Class carSelect
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(254, 355)
+        Me.Label9.Location = New System.Drawing.Point(173, 351)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(196, 13)
         Me.Label9.TabIndex = 10
@@ -157,7 +157,7 @@ Partial Class carSelect
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(254, 439)
+        Me.Label10.Location = New System.Drawing.Point(173, 435)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(233, 13)
         Me.Label10.TabIndex = 11
@@ -165,26 +165,26 @@ Partial Class carSelect
         '
         'lblPercent
         '
-        Me.lblPercent.Location = New System.Drawing.Point(247, 381)
+        Me.lblPercent.Location = New System.Drawing.Point(166, 377)
         Me.lblPercent.Name = "lblPercent"
         Me.lblPercent.Size = New System.Drawing.Size(252, 20)
         Me.lblPercent.TabIndex = 12
         '
         'lblCompPay
         '
-        Me.lblCompPay.Location = New System.Drawing.Point(246, 464)
+        Me.lblCompPay.Location = New System.Drawing.Point(165, 460)
         Me.lblCompPay.Name = "lblCompPay"
         Me.lblCompPay.Size = New System.Drawing.Size(251, 20)
         Me.lblCompPay.TabIndex = 13
         '
-        'Button1
+        'btnEnter
         '
-        Me.Button1.Location = New System.Drawing.Point(326, 597)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 14
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnEnter.Location = New System.Drawing.Point(246, 591)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 14
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
         '
         'RadioButton1
         '
@@ -222,7 +222,7 @@ Partial Class carSelect
         'carName
         '
         Me.carName.AutoSize = True
-        Me.carName.Location = New System.Drawing.Point(318, 116)
+        Me.carName.Location = New System.Drawing.Point(237, 112)
         Me.carName.Name = "carName"
         Me.carName.Size = New System.Drawing.Size(23, 13)
         Me.carName.TabIndex = 18
@@ -231,7 +231,7 @@ Partial Class carSelect
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(254, 521)
+        Me.Label11.Location = New System.Drawing.Point(173, 517)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(172, 13)
         Me.Label11.TabIndex = 19
@@ -242,7 +242,7 @@ Partial Class carSelect
         Me.Panel2.Controls.Add(Me.RadioButton3)
         Me.Panel2.Controls.Add(Me.RadioButton2)
         Me.Panel2.Controls.Add(Me.RadioButton1)
-        Me.Panel2.Location = New System.Drawing.Point(224, 547)
+        Me.Panel2.Location = New System.Drawing.Point(143, 543)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(297, 31)
         Me.Panel2.TabIndex = 0
@@ -251,11 +251,11 @@ Partial Class carSelect
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 702)
+        Me.ClientSize = New System.Drawing.Size(624, 642)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.carName)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnEnter)
         Me.Controls.Add(Me.lblCompPay)
         Me.Controls.Add(Me.lblPercent)
         Me.Controls.Add(Me.Label10)
@@ -279,16 +279,16 @@ Partial Class carSelect
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblCC As Label
+    Friend WithEvents lblCost As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Label9 As Label
     Friend WithEvents Label10 As Label
     Friend WithEvents lblPercent As TextBox
     Friend WithEvents lblCompPay As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnEnter As Button
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents RadioButton3 As RadioButton
