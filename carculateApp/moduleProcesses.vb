06ab5c916ce4d7cc853aeb0ba7c146d7603bff42
ahmadfarismk <ahmadfarismk@gmail.com>
2024-07-03 22:50:45 +0800

    processes calculation template = done by joe

---
+++ carculateApp/moduleProcesses.vb
@@ -0,0 +1,66 @@
+﻿Module moduleProcesses
+    Public Function calcYearExpenses(ByVal opr1 As Double,
+                                 ByVal opr2 As Double,
+                                 ByVal opr3 As Double) As Double
+        'Variable declaration
+        Dim yearExp As Double
+
+        yearExp = (0.01 * opr1) + (0.005 * opr1) + opr2 + opr3
+
+        Return yearExp
+
+    End Function
+
+    Public Function calcDownpayment(ByVal opr1 As Double,
+                                    ByVal opr2 As Double) As Double
+
+        'Variable declaration
+        Dim downPay As Double
+        downPay = opr1 * opr2
+
+        Return downPay
+    End Function
+
+
+    Public Function calcYearInstall(ByVal opr1 As Double,
+                                    ByVal opr2 As Double,
+                                    ByVal opr3 As Double) As Double
+
+        'Variable declaration
+        Dim yearInstall As Double
+        yearInstall = (opr1 - opr2) / opr2
+
+        Return yearInstall
+
+    End Function
+
+
+    Public Function calcTotYear(ByVal opr1 As Double,
+                                ByVal opr2 As Double) As Double
+        'Variable declaration
+        Dim totYear As Double
+        totYear = opr1 + opr2
+
+        Return totYear
+    End Function
+
+    Public Function calcMonthPay(ByVal opr1 As Double) As Double
+        'Variable declaration
+        Dim monthPay As Double
+        monthPay = opr1 / 12
+
+        Return monthPay
+    End Function
+
+    Public Function calcMinSal(ByVal opr1 As Double,
+                               ByVal opr2 As Double) As Double
+        'Variable declaration
+        Dim minSal As Double
+        minSal = opr1 / opr2
+
+        Return minSal
+    End Function
+
+
+
+End Module
--- carculateApp/output.vb
+++ carculateApp/output.vb
@@ -6,6 +6,7 @@ Public Class output
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Database Connection
         Dim dbconn As New System.Data.OleDb.OleDbConnection()
         dbconn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ahmad\Downloads\carculatedb[1].accdb"
         Dim Searchquery As String = "Select * from car where id=@id"
@@ -14,8 +15,33 @@ Public Class output
         Dim sql As New OleDbDataAdapter(cmd)
         Dim table As New DataTable
         sql.Fill(table)
+
+        'Variable declaration
+        Dim cc As Integer
+        Dim roadtax As Double = 0.00
+
+
+        cc = Convert.ToInt32(table.Rows(0)(2).ToString)
+
+        'CC and roadtax calculation
+        If (cc <= 1600) Then
+            roadtax = 200.0
+        ElseIf (cc <= 1800) Then
+            roadtax = 200.0 + (cc - 1600) * 0.4
+        ElseIf (cc <= 2000) Then
+            roadtax = 280.0 + (cc - 1800) * 0.5
+        ElseIf (cc <= 2500) Then
+            roadtax = 380.0 + (cc - 2000) * 1.0
+        ElseIf (cc <= 3000) Then
+            roadtax = 880.0 + (cc - 2500) * 2.5
+        Else
+            roadtax = 2130.0 + (cc - 3000) * 4.5
+        End If
+
+
         If table.Rows.Count > 0 Then
             lblDown.Text = table.Rows(0)(1).ToString
+            'lblInstallment.Text = 
         Else
             MessageBox.Show("Username and Password do not match.", "Authentication Failure", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
