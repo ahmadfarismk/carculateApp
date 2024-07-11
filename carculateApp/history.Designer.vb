8d827c213e41d94be9245604fb4653660fa38296
ahmadfarismk <ahmadfarismk@gmail.com>
2024-07-11 23:27:04 +0800

    Revert "a"
    This reverts commit f8710d45d7575e11c6c48971462abca9a728fa8a.

--- carculateApp/history.Designer.vb
+++ carculateApp/history.Designer.vb
@@ -22,18 +22,10 @@ Partial Class history
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(history))
         Me.Label4 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.HistoryBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.CarculatedbDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.CarculatedbDataSet = New carculateApp.carculatedbDataSet()
-        Me.HistoryTableAdapter = New carculateApp.carculatedbDataSetTableAdapters.historyTableAdapter()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.HistoryBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.CarculatedbDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.CarculatedbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label4
@@ -56,43 +48,21 @@ Partial Class history
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(785, 86)
+        Me.PictureBox1.Size = New System.Drawing.Size(800, 86)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
-        '
-        'HistoryBindingSource
-        '
-        Me.HistoryBindingSource.DataMember = "history"
-        Me.HistoryBindingSource.DataSource = Me.CarculatedbDataSetBindingSource
-        '
-        'CarculatedbDataSetBindingSource
-        '
-        Me.CarculatedbDataSetBindingSource.DataSource = Me.CarculatedbDataSet
-        Me.CarculatedbDataSetBindingSource.Position = 0
-        '
-        'CarculatedbDataSet
-        '
-        Me.CarculatedbDataSet.DataSetName = "carculatedbDataSet"
-        Me.CarculatedbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-        '
-        'HistoryTableAdapter
-        '
-        Me.HistoryTableAdapter.ClearBeforeFill = True
         '
         'history
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(785, 540)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "history"
         Me.Text = "history"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.HistoryBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.CarculatedbDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.CarculatedbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -100,8 +70,4 @@ Partial Class history
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents CarculatedbDataSetBindingSource As BindingSource
-    Friend WithEvents CarculatedbDataSet As carculatedbDataSet
-    Friend WithEvents HistoryBindingSource As BindingSource
-    Friend WithEvents HistoryTableAdapter As carculatedbDataSetTableAdapters.historyTableAdapter
 End Class
